7882ad5799d3d44436c963552bae9e978e5501e6
lllzzzkkk-s <54101973+lllzzzkkk-s@users.noreply.github.com>
2019-08-14 18:42:33 +0800

    Create chat.py

---
+++ chat.py
@@ -0,0 +1,188 @@
+from rasa_nlu.training_data import load_data
+from rasa_nlu.model import Trainer
+from rasa_nlu import config
+from states_rules import *
+
+from datetime import datetime
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+
+# iexfinance
+from iexfinance.stocks import Stock
+from iexfinance.stocks import get_historical_data
+
+# weather
+from chatbot2 import byHour
+from chatbot2 import y
+from chatbot5 import data
+
+a="The highest price is {}, What else do you want to know?"
+b="The lowest price is {}, What else do you want to know?"
+
+class Chatbot:
+
+    def __init__(self):
+        print('Create a new chatbot.')
+        trainer = Trainer(config.load("config_spacy.yml"))
+        # Load the training data
+        training_data = load_data('demo-rasa.json')
+        interpreter = trainer.train(training_data)
+        self.interpreter = interpreter
+        self.state = INIT
+        self.params={}
+
+    def send_message(self,state, pending, message):
+        print("old_state: ", state, "message: ", message, "pending: ", pending)
+        new_state, response, pending_state = self.respond(state, message)
+        #print("new_state: ", new_state, "response: ", response, "pending_state: ", pending_state)
+
+        if pending is not None:
+            new_state, response, pending_state = self.respond(state, message)
+        if pending_state is not None:
+            pending = (pending_state, self.get_intent(message))
+
+        return new_state, pending, response, self.get_intent(message)
+
+    def respond(self,state, message):
+        entity = self.get_entity(message)
+
+        try:
+            new_state = policy_rules[(state, self.get_intent(message))][0]
+            print("new_state is:"+str(new_state))
+        except KeyError:
+            new_state = CONFUSE
+        pending_state = policy_rules[(state, self.get_intent(message))][2]
+
+        if self.get_intent(message) == 'greet':
+            response = policy_rules[(state, self.get_intent(message))][1]
+        if self.get_intent(message) == 'highest':
+            response = a.format(data['high'])
+        if self.get_intent(message) == 'lowest':
+            response = b.format(data['low'])
+        if self.get_intent(message) == 'finish':
+            response = policy_rules[(state, self.get_intent(message))][1]
+
+        if self.get_intent(message) == 'query_weather':
+            response = policy_rules[(state, self.get_intent(message))][1]
+
+        if self.get_intent(message) == 'summary':
+            response = policy_rules[(state, self.get_intent(message))][1].format(byHour.summary)
+
+        if self.get_intent(message) == 'icon':
+            response = policy_rules[(state, self.get_intent(message))][1].format(byHour.icon)
+
+        if self.get_intent(message) == '48_temperature':
+            response = policy_rules[(state, self.get_intent(message))][1].format(y)
+
+        if self.get_intent(message) == 'weather_plot':
+            response = policy_rules[(state, self.get_intent(message))][1]
+
+        if self.get_intent(message) == 'function_intro':
+            response = policy_rules[(state, self.get_intent(message))][1]
+
+
+        if self.get_intent(message) == 'current_price':
+            response = policy_rules[(state, self.get_intent(message))][1].format(entity, self.get_current_price(entity), entity,
+                                                                            self.get_news(entity))
+        if self.get_intent(message) == 'clear_historical_data':
+            response = policy_rules[(state, self.get_intent(message))][1]
+            self.generate_figure(message)
+
+
+        if self.get_intent(message) == 'analyze':
+            response = policy_rules[(state, self.get_intent(message))][1].format(entity, self.get_ttmEPS(entity))
+
+        return new_state, response, pending_state
+
+    def get_intent(self,message):
+        return self.interpreter.parse(message)['intent']['name']
+
+
+
+    def get_entity(self,message):
+        if self.interpreter.parse(message)['entities'] == []:
+            return []
+
+        if self.interpreter.parse(message)['entities'][0]['entity'] == 'company':
+            return self.interpreter.parse(message)['entities'][0]['value']
+
+            return [self.interpreter.parse(message)['entities'][0]['value'],
+                    self.interpreter.parse(message)['entities'][1]['value']]
+
+    #########################################
+    def get_current_price(self,company):
+        print("Company: ", company)
+        stock = Stock(company, token="pk_77f74e3b59e54d43b48e72de351caaf0")
+        prices = str(stock.get_price())
+        return prices
+
+    def get_ttmEPS(self,company):
+        ttmEPS = Stock(company, token="pk_77f74e3b59e54d43b48e72de351caaf0").get_key_stats()['ttmEPS']
+        return ttmEPS
+
+    def get_news(self,company):
+        news = Stock(company, token="pk_77f74e3b59e54d43b48e72de351caaf0").get_news()
+        for i in news:
+            if i['summary'] != 'No summary available.':
+                return i['url']
+
+    def generate_figure(self,message,):
+        comprehended_data = self.interpreter.parse(message)
+
+        for i in range(0, 2):
+            if comprehended_data['entities'][i]['entity'] == 'company':
+                required_company = comprehended_data['entities'][i]['value']
+            if comprehended_data['entities'][i]['entity'] == 'his_price_type':
+                required_type = comprehended_data['entities'][i]['value']
+            else:
+                required_company = 'AAPL'
+                required_type = 'close'
+
+        if len(comprehended_data['entities']) <= 3:
+            time_period = [comprehended_data['entities'][0]['value'],
+                           comprehended_data['entities'][1]['value']]
+
+            start_time_splited = time_period[0].split(' - ')
+            end_time_splited = time_period[1].split(' - ')
+
+            start_year = int(start_time_splited[0])
+            start_month = int(start_time_splited[1])
+            start_day = int(start_time_splited[2])
+
+            end_year = int(end_time_splited[0])
+            end_month = int(end_time_splited[1])
+            end_day = int(end_time_splited[2])
+
+            start_time = datetime(start_year, start_month, start_day)
+            end_time = datetime(end_year, end_month, end_day)
+
+            his_data = get_historical_data(required_company, start_time, end_time, output_format='pandas', token="pk_77f74e3b59e54d43b48e72de351caaf0")
+
+        else:
+            time_period = [comprehended_data['entities'][2]['value'],
+                           comprehended_data['entities'][3]['value']]
+
+            start_time_splited = time_period[0].split('-')
+            end_time_splited = time_period[1].split('-')
+
+            start_year = int(start_time_splited[0])
+            start_month = int(start_time_splited[1])
+            start_day = int(start_time_splited[2])
+
+            end_year = int(end_time_splited[0])
+            end_month = int(end_time_splited[1])
+            end_day = int(end_time_splited[2])
+
+            start_time = datetime(start_year, start_month, start_day)
+            end_time = datetime(end_year, end_month, end_day)
+
+            his_data = get_historical_data(required_company, start_time, end_time, output_format='pandas', token="pk_77f74e3b59e54d43b48e72de351caaf0")
+
+        plot_required_type = his_data[required_type].plot()
+        fig = plot_required_type.get_figure()
+        fig.savefig('result.png')
+
+
+
